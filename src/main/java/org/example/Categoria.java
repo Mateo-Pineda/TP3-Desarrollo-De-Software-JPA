4146129e798019d0fe9afc245cf7dc0a7cee2f19
Mateo Pineda <mateo08112002@gmail.com>
2024-09-08 20:36:50 -0300

    first commit

---
+++ src/main/java/org/example/Categoria.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Categoría")
+public class Categoria implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Denominación")
+    private String denominacion;
+
+    public Categoria() {
+    }
+
+    public Categoria(String denominacion) {
+        this.denominacion = denominacion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDenominacion() {
+        return denominacion;
+    }
+
+    public void setDenominacion(String denominacion) {
+        this.denominacion = denominacion;
+    }
+}
